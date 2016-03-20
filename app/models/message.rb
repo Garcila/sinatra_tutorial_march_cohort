736680169e55ae489f1ebcaa0b6495f82964e651
German Arcila <germanarcila@gmail.com>
2016-03-20 13:20:48 -0400

    Needs last step other posts from this author

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,6 @@ class Message < ActiveRecord::Base
 
 	validates :content, presence: true, length: {maximum: 140}
 	validates :author, presence: true, length: {maximum: 25}
+	validates_format_of :url, :with => URI::regexp(%w(http https))
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319223716) do
+ActiveRecord::Schema.define(version: 20160320154337) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "author"
     t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
 end
